bddfb970b138dec7b251b0772522470f90f6da49
Prieyudha Akadita S <akaditasustono@gmail.com>
2021-01-02 19:43:27 +0700

    Added has many relation

--- repository/user-repository.go
+++ repository/user-repository.go
@@ -70,7 +70,7 @@ func (db *userConnection) FindByEmail(email string) entity.User {
 
 func (db *userConnection) ProfileUser(userID string) entity.User {
 	var user entity.User
-	db.connection.Find(&user, userID)
+	db.connection.Preload("Books").Preload("Books.User").Find(&user, userID)
 	return user
 }
 
